b9ade78c7afdea1aacc4a82590596c47b97a2496
Sandeep <wd.sandeep@gmail.com>
2021-10-30 22:07:58 +0530

    Custom directives (ClickOutside and PageScroll) and using debouncing from lodash

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="header" id="header">
+  <header class="header" id="header"
+  v-click-outside="() => mobileNavMenu = false"
+  v-page-scroll="() => mobileNavMenu = false"
+  >
 
     <router-link :to="{ name: 'Home' }" class="logo">
       <img src="@/assets/svg/vueschool-logo.svg">
     </router-link>
 
-    <div class="btn-hamburger">
+    <div class="btn-hamburger" @click="mobileNavMenu = !mobileNavMenu">
       <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
@@ -13,12 +16,15 @@
     </div>
 
     <!-- use .navbar-open to open nav -->
-    <nav class="navbar">
+    <nav class="navbar" :class="{'navbar-open': mobileNavMenu}">
 
       <ul>
 
         <li v-if="authUser" class="navbar-user">
-          <a @click.prevent="userDropDownOpen = !userDropDownOpen">
+          <a
+          v-click-outside="() => userDropDownOpen = false"
+          @click.prevent="userDropDownOpen = !userDropDownOpen"
+          >
             <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} profile picture`"/>
             <span>
                 {{ authUser.name }}
@@ -32,13 +38,24 @@
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item"><router-link :to="{name: 'Profile' }">View profile</router-link></li>
-              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
+              <li class="dropdown-menu-item"><a
+              @click.prevent="$store.dispatch('auth/signOut'),
+              $router.push({name: 'Home'}),
+              mobileNavMenu = false"
+              >Sign Out</a></li>
             </ul>
           </div>
         </li>
 
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'SignIn' }">Sign In</router-link></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'Register' }">Register</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item"><router-link :to="{name: 'Profile' }">View profile</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item">
+          <a
+          @click.prevent="$store.dispatch('auth/signOut'),
+          $router.push({name: 'Home'}),
+          mobileNavMenu = false"
+          >Sign Out</a></li>
       </ul>
 
 <!--      <ul>-->
@@ -73,11 +90,17 @@ export default {
   name: 'TheNavbar',
   data () {
     return {
-      userDropDownOpen: false
+      userDropDownOpen: false,
+      mobileNavMenu: false
     }
   },
   computed: {
     ...mapGetters('auth', ['authUser'])
+  },
+  created () {
+    this.$router.beforeEach((to, from) => {
+      this.mobileNavMenu = false
+    })
   }
 }
 </script>
---
+++ src/plugins/ClickOutsideDirective.js
@@ -0,0 +1,18 @@
+const ClickOutsideDirective = {
+  mounted (el, binding) {
+    el.__clickOutsideHandler__ = event => {
+      console.log(el)
+      console.log(event.target)
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event)
+      }
+    }
+    document.body.addEventListener('click', el.__clickOutsideHandler__)
+  },
+  unmounted (el) {
+    document.body.addEventListener('click', el.__clickOutsideHandler__)
+  }
+}
+export default (app) => {
+  app.directive('click-outside', ClickOutsideDirective)
+}
