d9baa5ce96e8c5a28dc1a99690af3acd7582d458
wdsandeep <wd.sandeep@gmail.com>
2021-11-03 12:53:18 +0530

    Recommiting because netlify build is showing error for config/firebase.js file, changing .gitignore

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyAPrq0wrK7R_i7_GDLWKdDMm8szbHq1UbI',
+  authDomain: 'vue-school-forum-ac68d.firebaseapp.com',
+  projectId: 'vue-school-forum-ac68d',
+  storageBucket: 'vue-school-forum-ac68d.appspot.com',
+  messagingSenderId: '340872525017',
+  appId: '1:340872525017:web:e98669194d61d64ea9bac8'
+}
